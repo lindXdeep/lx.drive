88525a644bcb3f905c33d6b986ae8a1915dfcd6c
lindx <linder.igor@gmail.com>
2021-12-13 03:53:12 +0300

    modules project

---
+++ client/src/main/java/lx/lindx/drive/client/App.java
@@ -0,0 +1,11 @@
+package lx.lindx.drive.client;
+
+/**
+ * App
+ */
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("client");
+  }
+}
---
+++ server/src/main/java/lx/lindx/drive/server/App.java
@@ -0,0 +1,11 @@
+package lx.lindx.drive.server;
+
+/**
+ * App
+ */
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("server");
+  }
+}
