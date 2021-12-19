497215a7cf95f7039220a4e3587a45af79d625a8
lindx <linder.igor@gmail.com>
2021-12-19 08:01:21 +0300

    add protocol data transfer for client and server

--- server/src/main/java/lx/lindx/drive/server/security/Authorize.java
+++ server/src/main/java/lx/lindx/drive/server/security/Authorize.java
@@ -1,24 +1,33 @@
 package lx.lindx.drive.server.security;
 
 import lx.lindx.drive.server.Protocol;
+import lx.lindx.drive.server.util.Config;
 import lx.lindx.drive.server.util.Util;
 
 public class Authorize {
 
   private Protocol protocol;
 
+  private byte[] buffer;
+
+  private String command;
+  private String data;
+  private int dataSize;
+
   public Authorize(Protocol protocol) {
     this.protocol = protocol;
+    this.buffer = new byte[Config.getBufSize()];
   }
 
   public boolean authorize(byte[] buf) {
 
-    byte[][] b = protocol.unpacked(buf);
+    command = Util.byteToStr(protocol.unpacked(buf)[0]);
+    data = Util.byteToStr(protocol.unpacked(buf)[2]);
+    dataSize = Util.byteToInt(protocol.unpacked(buf)[1]);
 
-    System.out.println(Util.byteToStr(b[0])); // command
-    System.out.println(Util.byteToInt(b[1])); // data size
-    System.out.println(Util.byteToStr(b[2])); // data
+    Util.log("get command: " + command + " data: " + data + " size: " + dataSize + "byte");
 
+  
     return false;
   }
 }
